e3d7c94a23d2ec7114cdf96581c7f97bc5805b27
rushabhjsoni <slayer.r1@gmail.com>
2021-10-27 18:58:05 +0530

    completed 2 steps out of 3

--- src/HornedBeasts.js
+++ src/HornedBeasts.js
@@ -1,14 +1,29 @@
 import { Component } from "react";
 
+
 class HornedBeasts extends Component {
   
+    constructor(props) {
+      super(props);
+      this.state = {
+        numberOfFavorites: 0
+      }
+    }
+
+    handleClick = () => {
+      this.setState({numberOfFavorites:this.state.numberOfFavorites + 1});
+    }
+
+
+
   render() {
     return(
-      <>
-    <h2>{this.props.title}</h2>
-    <img src={this.props.imageUrl}/>
-    <p>{this.props.desc}</p>
-    </>
+    <div>
+    {this.props.title}
+    <img src={`${this.props.img}`} alt={''} onClick={this.handleClick}/> 
+    <p>{this.props.fav} {this.state.numberOfFavorites}</p>
+    {this.props.des}
+    </div>
     )
   }
 }
